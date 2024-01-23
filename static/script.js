0cfea68613ba0e562226361aa2b7270f91ca7c71
Brando <bbaker.sh@gmail.com>
2024-01-23 15:56:43 -0800

    added JQ logic to capture input

--- static/script.js
+++ static/script.js
@@ -1,1 +1,11 @@
-console.log('app.js loaded');
+console.log("app.js loaded");
+
+$guessForm = $("#guess");
+
+$guessForm.on("submit", function (event) {
+  event.preventDefault(); // prevent the form from submitting
+
+  let submittedGuess = $("#guess-input").val();
+  console.log(submittedGuess);
+  console.log("guess form submitted");
+});
